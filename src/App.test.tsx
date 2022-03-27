34b055a376e61642e817841c30bca69a85a4a00f
shoaib <shoaibkhurshid9@gmail.com>
2022-03-27 19:04:31 +0500

    usestate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+const {show, setshow}= useState(false);
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
@@ -10,4 +11,9 @@ test('renders learn react link', () => {
 
 
 
+
+
 /////Shoaib Khurshid
+
+
+/////
